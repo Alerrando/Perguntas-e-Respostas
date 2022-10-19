0a1070153c92cd3a3520a475d84a5c42697f4a83
Alerrando Breno <alerrando2@gmail.com>
2022-10-19 00:39:10 -0300

    add variaveis

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const app = express();
 // Estou dizendo ao Express usar o EJS como view engine
 app.set("view engine", "ejs");
 
-app.get("/home", (req, res) => {
-    res.render("index")
+app.get("/", (req, res) => {
+    let nome = "Alerrando Breno";
+    let lang = "Javascript";
+
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        empresa: "Guia do Programador",
+        idade: 19,
+    })
 })
 
 app.listen(8000, () => {
     console.log("App rodando!");
 });
-
