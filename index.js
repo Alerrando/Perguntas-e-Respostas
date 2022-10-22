76cd47234ad3d21e6422b98115043f8f10f8009f
Alerrando Breno <alerrando2@gmail.com>
2022-10-22 12:36:30 -0300

    folder css

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ const app = express();
 
 // Estou dizendo ao Express usar o EJS como view engine
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    let nome = "Alerrando Breno";
-    let lang = "Javascript";
+app.get("/:nome/:lang", (req, res) => {
+    let {nome, lang} = req.params
+    let exibirMSG = true;
+
+    let produtos = [
+        {nome: "Doritos", preco: 3.14},
+        {nome: "Coca-Cola", preco: 5},
+        {nome: "Leite", preco:1.45},
+    ];
 
     res.render("index", {
         nome: nome,
         lang: lang,
         empresa: "Guia do Programador",
         idade: 19,
+        msg: exibirMSG,
+        produtos: produtos,
     })
 })
 
