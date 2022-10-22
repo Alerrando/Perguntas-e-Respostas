18e46ab26cff457fc7f47ccb2cff1e9995548e26
Alerrando Breno <alerrando2@gmail.com>
2022-10-22 13:06:49 -0300

    add bootstrap

--- index.js
+++ index.js
@@ -5,24 +5,10 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/:nome/:lang", (req, res) => {
-    let {nome, lang} = req.params
-    let exibirMSG = true;
+app.get("/", (req, res) => {
 
-    let produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca-Cola", preco: 5},
-        {nome: "Leite", preco:1.45},
-    ];
 
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do Programador",
-        idade: 19,
-        msg: exibirMSG,
-        produtos: produtos,
-    })
+    res.render("index")
 })
 
 app.listen(8000, () => {
