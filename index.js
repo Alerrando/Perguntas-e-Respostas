ab8401006c8eafa84e59806c2514c294d1f00444
Alerrando Breno <alerrando2@gmail.com>
2022-10-23 16:17:31 -0300

    add form

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-
-
     res.render("index")
+})
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar")
 })
 
 app.listen(8000, () => {
