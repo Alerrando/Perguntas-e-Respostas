12e542d6a9e75dd17f6e669a8d2612d3cae59002
Alerrando Breno <alerrando2@gmail.com>
2022-10-23 21:35:55 -0300

    add folder database

---
+++ database/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require("sequelize");
+
+const connection = new Sequelize("guiaperguntas", "root", "147258", {
+    host: 'localhost',
+    dialect:'sqlite',
+});
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("/database/database");
+
 
 // Estou dizendo ao Express usar o EJS como view engine
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-    res.render("index")
-})
+    res.render("index");
+});
 
 app.get("/perguntar", (req, res) => {
-    res.render("perguntar")
-})
+    res.render("perguntar");
+});
 
+app.post("/salvarpergunta", (req, res) => {
+    let { titulo, descrição } = req.body
+    
+    res.send(`Formulário recebido ${titulo}, ${descrição}`);
+});
 
 app.listen(8000, () => {
     console.log("App rodando!");
